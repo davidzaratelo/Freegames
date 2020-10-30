0325f8b6ee41752927c12f6199dce4012b50e5c0
Alejandro López <62221946+alexlohe25@users.noreply.github.com>
2020-10-30 09:39:18 -0600

    Add files via upload

--- paint.py
+++ paint.py
@@ -9,15 +9,18 @@ Exercises
 
 from turtle import *
 from freegames import vector
-
+#funcion que traza una línea de determinada longitud dados un punto inicial
+#y uno final
 def line(start, end):
-    color('green')
     "Draw line from start to end."
     up()
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
-
+'''funcion que traza un cuadrado de determinada longitud de lado 
+    dados un punto inicial y uno final a traves de un for en donde
+    cada iteracion traza una linea y después gira 90 grados
+'''
 def square(start, end):
     "Draw square from start to end."
     up()
@@ -30,7 +33,9 @@ def square(start, end):
         left(90)
 
     end_fill()
-
+''' Funcion que traza un circulo (enorme generalmente) a través de trazar 360 veces una linea
+    y por cada iteración girar 1 grado a la izquierda
+'''
 def circle(start, end):
     up()
     goto(start.x, start.y)
@@ -42,11 +47,26 @@ def circle(start, end):
         left(1)
 
     end_fill()
-
+'''Funcion que traza un rectángulo de largo determinado por un punto final y uno inicial
+    y el ancho determinado por la mitad del largo
+'''
 def rectangle(start, end):
-    "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    #for, que determina si el ancho o el largo se va a dibujar segun la iteracion
+    for count in range(4):
+        if (count % 2 == 0):
+            forward(end.x - start.x)
+        else:
+            forward((end.x / 2) - (start.x / 2))
+        left(90)
 
+    end_fill()
+'''funcion que traza un triángulo equilátero de longitud determianda por un punto final y uno inicial
+    a través de un for que traza una linea y gira 120 a la izquierda (60°) después de trazar
+'''
 def triangle(start, end):
     up()
     goto(start.x, start.y)
@@ -85,6 +105,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: width(10), 'w')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
